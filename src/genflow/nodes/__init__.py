29e4fe682be382e850aeed5859d8237b64d8b64b
Matthias Georgi <mmg@meta.com>
2024-03-22 13:45:51 +0100

    always load nodes

--- src/genflow/nodes/__init__.py
+++ src/genflow/nodes/__init__.py
@@ -29,56 +29,51 @@ import genflow.nodes.genflow.text.vector
 import genflow.nodes.genflow.video
 from genflow.workflows.workflow_node import WorkflowNode
 
+import genflow.nodes.comfy
+import genflow.nodes.comfy.advanced
+import genflow.nodes.comfy.advanced.conditioning
+import genflow.nodes.comfy.advanced.loaders
+import genflow.nodes.comfy.conditioning
+import genflow.nodes.comfy.controlnet
+import genflow.nodes.comfy.controlnet.faces_and_poses
+import genflow.nodes.comfy.controlnet.semantic_segmentation
+import genflow.nodes.comfy.controlnet.normal_and_depth
+import genflow.nodes.comfy.controlnet.others
+import genflow.nodes.comfy.controlnet.line_extractors
+import genflow.nodes.comfy.controlnet.t2i
+import genflow.nodes.comfy.generate
+import genflow.nodes.comfy.image
+import genflow.nodes.comfy.image.animation
+import genflow.nodes.comfy.image.batch
+import genflow.nodes.comfy.image.preprocessors
+import genflow.nodes.comfy.image.transform
+import genflow.nodes.comfy.image.upscaling
+import genflow.nodes.comfy.ipadapter
+import genflow.nodes.comfy.latent
+import genflow.nodes.comfy.latent.advanced
+import genflow.nodes.comfy.latent.batch
+import genflow.nodes.comfy.latent.inpaint
+import genflow.nodes.comfy.latent.transform
+import genflow.nodes.comfy.loaders
+import genflow.nodes.comfy.mask
+import genflow.nodes.comfy.mask.compositing
+import genflow.nodes.comfy.sampling
+import genflow.nodes.comfy.sampling.samplers
+import genflow.nodes.comfy.sampling.schedulers
+import genflow.nodes.comfy.sampling.sigmas
 
-if Environment.get_comfy_folder():
-    import genflow.nodes.comfy
-    import genflow.nodes.comfy.advanced
-    import genflow.nodes.comfy.advanced.conditioning
-    import genflow.nodes.comfy.advanced.loaders
-    import genflow.nodes.comfy.conditioning
-    import genflow.nodes.comfy.controlnet
-    import genflow.nodes.comfy.controlnet.faces_and_poses
-    import genflow.nodes.comfy.controlnet.semantic_segmentation
-    import genflow.nodes.comfy.controlnet.normal_and_depth
-    import genflow.nodes.comfy.controlnet.others
-    import genflow.nodes.comfy.controlnet.line_extractors
-    import genflow.nodes.comfy.controlnet.t2i
-    import genflow.nodes.comfy.generate
-    import genflow.nodes.comfy.image
-    import genflow.nodes.comfy.image.animation
-    import genflow.nodes.comfy.image.batch
-    import genflow.nodes.comfy.image.preprocessors
-    import genflow.nodes.comfy.image.transform
-    import genflow.nodes.comfy.image.upscaling
-    import genflow.nodes.comfy.ipadapter
-    import genflow.nodes.comfy.latent
-    import genflow.nodes.comfy.latent.advanced
-    import genflow.nodes.comfy.latent.batch
-    import genflow.nodes.comfy.latent.inpaint
-    import genflow.nodes.comfy.latent.transform
-    import genflow.nodes.comfy.loaders
-    import genflow.nodes.comfy.mask
-    import genflow.nodes.comfy.mask.compositing
-    import genflow.nodes.comfy.sampling
-    import genflow.nodes.comfy.sampling.samplers
-    import genflow.nodes.comfy.sampling.schedulers
-    import genflow.nodes.comfy.sampling.sigmas
+import genflow.nodes.huggingface.audio.generate
+import genflow.nodes.huggingface.image.generate
+import genflow.nodes.huggingface.image.classify
+import genflow.nodes.huggingface.text.classify
+import genflow.nodes.huggingface.text.summarize
+import genflow.nodes.huggingface.text.generate
 
-if Environment.get_huggingface_token():
-    import genflow.nodes.huggingface.audio.generate
-    import genflow.nodes.huggingface.image.generate
-    import genflow.nodes.huggingface.image.classify
-    import genflow.nodes.huggingface.text.classify
-    import genflow.nodes.huggingface.text.summarize
-    import genflow.nodes.huggingface.text.generate
+import genflow.nodes.openai.audio
+import genflow.nodes.openai.image
+import genflow.nodes.openai.text
 
-if Environment.get_openai_api_key():
-    import genflow.nodes.openai.audio
-    import genflow.nodes.openai.image
-    import genflow.nodes.openai.text
-
-if Environment.get_replicate_api_token():
-    import genflow.nodes.replicate.audio.generate
-    import genflow.nodes.replicate.audio.transcribe
-    import genflow.nodes.replicate.image.generate
-    import genflow.nodes.replicate.text.generate
+import genflow.nodes.replicate.audio.generate
+import genflow.nodes.replicate.audio.transcribe
+import genflow.nodes.replicate.image.generate
+import genflow.nodes.replicate.text.generate
