d29bd669742be3674a29eee760280f573d22b4e4
Matthias Georgi <matti.georgi@gmail.com>
2024-05-05 10:55:26 +0200

    add sentry for react

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -38,6 +38,21 @@ import AppFooter from "./components/panels/AppFooter";
 import OAuthCallback from "./components/OauthCallback";
 import ExampleGrid from "./components/workflows/ExampleGrid";
 import OpenOrCreateDialog from "./components/dialogs/OpenOrCreateDialog";
+import * as Sentry from "@sentry/react";
+
+if (import.meta.env.MODE === "production") {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration()
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: [/^https:\/\/app.nodetool\.ai/],
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
 
 const queryClient = new QueryClient();
 useAssetStore.getState().setQueryClient(queryClient);
