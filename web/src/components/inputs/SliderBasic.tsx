73c7b0aa9d2e2faaa204c84f058293fd1cff3245
heavy-d <david@reaktant.com>
2024-05-26 01:07:15 +0200

    SliderBasic style

--- web/src/components/inputs/SliderBasic.tsx
+++ web/src/components/inputs/SliderBasic.tsx
@@ -2,43 +2,45 @@
 import { css } from "@emotion/react";
 import { Slider, SliderProps, Tooltip } from "@mui/material";
 
-const sliderBasicStyles = (theme: any) => ({
-  "&": {
-    marginTop: "3px"
-  },
-  ".MuiSlider-rail": {
-    backgroundColor: theme.palette.c_gray3,
-    borderRadius: "0px",
-    height: "5px"
-  },
-  ".MuiSlider-track": {
-    height: "5px",
-    opacity: "1",
-    left: "0",
-    borderRadius: "0px"
-  },
-  ".MuiSlider-thumb": {
-    backgroundColor: theme.palette.c_hl1,
-    boxShadow: "0px 0px 5px 1px rgba(0, 0, 0, 0.25)",
-    borderRadius: "0",
-    width: "8px",
-    height: "8px",
-    "&:hover, &:focus, &:active": {
+const sliderBasicStyles = (theme: any) =>
+  css({
+    "&": {
+      marginTop: "3px",
+      padding: "0"
+    },
+    ".MuiSlider-rail": {
+      backgroundColor: theme.palette.c_gray3,
+      borderRadius: "0px",
+      height: "5px"
+    },
+    ".MuiSlider-track": {
+      height: "5px",
+      opacity: "1",
+      left: "0",
+      borderRadius: "0px"
+    },
+    ".MuiSlider-thumb": {
+      backgroundColor: theme.palette.c_gray5,
       boxShadow: "0px 0px 5px 1px rgba(0, 0, 0, 0.25)",
-      backgroundColor: "#666"
-    },
-    "&.Mui-focusVisible": {
-      boxShadow: "0px 0px 5px 1px rgba(0, 0, 0, 0.25)"
-    },
-    "&.Mui-active": {
-      boxShadow: "0px 0px 5px 1px rgba(0, 0, 0, 0.25)"
-    },
-    "&::before, &::after": {
-      width: "12px",
-      height: "12px"
+      borderRadius: "0",
+      width: "8px",
+      height: "8px",
+      "&:hover, &:focus, &:active": {
+        boxShadow: "0px 0px 5px 1px rgba(0, 0, 0, 0.25)",
+        backgroundColor: theme.palette.c_hl1
+      },
+      "&.Mui-focusVisible": {
+        boxShadow: "0px 0px 5px 1px rgba(0, 0, 0, 0.25)"
+      },
+      "&.Mui-active": {
+        boxShadow: "0px 0px 5px 1px rgba(0, 0, 0, 0.25)"
+      },
+      "&::before, &::after": {
+        width: "12px",
+        height: "12px"
+      }
     }
-  }
-});
+  });
 
 type SliderBasicProps = SliderProps & {
   tooltipText?: string;
