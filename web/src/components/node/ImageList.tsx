780b8363fadb9434d6434e46f5799c8bed411573
heavy-d <david@reaktant.com>
2024-07-07 00:45:48 +0200

    ImageList as grid layout

--- web/src/components/node/ImageList.tsx
+++ web/src/components/node/ImageList.tsx
@@ -1,9 +1,27 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
 import React from "react";
 import { ImageRef } from "../../stores/ApiTypes";
 
-const ImageList: React.FC<{ images: Array<ImageRef>; }> = ({ images }) => {
+const styles = (theme: any) =>
+  css({
+    "&": {
+      maxHeight: "500px",
+      overflow: "auto",
+      display: "grid",
+      gridTemplateColumns: "repeat(4, 1fr)",
+      gap: ".1em",
+      padding: "0"
+    },
+    "& img": {
+      width: "100%"
+    }
+  });
+
+const ImageList: React.FC<{ images: Array<ImageRef> }> = ({ images }) => {
   return (
-    <div>
+    <div className="image-list" css={styles}>
       {images.map((image) => (
         <img key={image.uri} src={image.uri} alt="" width="150" />
       ))}
