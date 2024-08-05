8c1afca73381397ab029695dae9eeb0718917038
heavy-d <david@reaktant.com>
2024-08-05 13:52:36 +0200

    change handling of broken example workflows: don't break, show empty workflow with ERROR added to the name - indicating the problematic workflow

--- src/nodetool/workflows/examples.py
+++ src/nodetool/workflows/examples.py
@@ -1,25 +1,45 @@
-from datetime import datetime
 import json
 import os
-from typing import Any
+import logging
+from datetime import datetime
+from typing import Any, List
 from nodetool.types.workflow import Workflow
 from nodetool.types.graph import Graph
 from nodetool.workflows.read_graph import read_graph
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 examples_folder = os.path.join(os.path.dirname(__file__), "examples")
 examples = None
 
-
 def load_example(name: str) -> Workflow:
-    with open(os.path.join(examples_folder, name), "r") as f:
-        props = json.loads(f.read())
-        if not "nodes" in props["graph"]:
-            edges, nodes = read_graph(props["graph"])
-            props["graph"] = Graph(edges=edges, nodes=nodes)
-        return Workflow(**props)
+    example_path = os.path.join(examples_folder, name)
+    with open(example_path, "r") as f:
+        try:
+            content = f.read()
+            props = json.loads(content)
+            if not "nodes" in props["graph"]:
+                edges, nodes = read_graph(props["graph"])
+                props["graph"] = Graph(edges=edges, nodes=nodes)
+            return Workflow(**props)
+        except json.JSONDecodeError as e:
+            logger.error(f"Error decoding JSON for example workflow {name}: {e}")
+            logger.error(f"Content of {name}: {content}")
+            # Return an empty Workflow with the name indicating it is broken
+            now_str = datetime.now().isoformat()
+            return Workflow(
+                id="",
+                name=f"[ERROR] {name}",
+                graph=Graph(nodes=[], edges=[]),
+                access="",
+                created_at=now_str,
+                updated_at=now_str,
+                description="Error loading this workflow"
+            )
 
-
-def get_examples() -> list[Workflow]:
+def get_examples() -> List[Workflow]:
     global examples
     if examples is None:
         examples = [
