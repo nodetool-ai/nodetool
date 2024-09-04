8be33bc145c0979bc52f0b3f926a681f2b00d6e4
heavy-d <david@reaktant.com>
2024-09-05 01:17:06 +0200

    add displayName to fix linter

--- web/src/components/node_menu/RenderNodes.tsx
+++ web/src/components/node_menu/RenderNodes.tsx
@@ -47,59 +47,59 @@ const nodeStyles = (theme: any) =>
     }
   });
 
-const NodeItem: React.FC<NodeItemProps> = memo(({
-  node,
-  isHovered,
-  onMouseEnter,
-  onMouseLeave,
-  onDragStart,
-  onClick
-}: NodeItemProps) => {
-  const outputType = node.outputs.length > 0 ? node.outputs[0].type.type : "";
+const NodeItem: React.FC<NodeItemProps> = memo(
+  ({
+    node,
+    isHovered,
+    onMouseEnter,
+    onMouseLeave,
+    onDragStart,
+    onClick
+  }: NodeItemProps) => {
+    const outputType = node.outputs.length > 0 ? node.outputs[0].type.type : "";
 
-  return (
-    <div
-      className={`node ${isHovered ? "hovered" : ""}`}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      draggable
-      onDragStart={onDragStart}
-    >
-      {iconForType(outputType, {
-        fill: "#fff",
-        containerStyle: {
-          borderRadius: "0 0 3px 0",
-          marginLeft: "0.1em",
-          marginTop: "0"
-        },
-        bgStyle: {
-          backgroundColor: "#333",
-          margin: "0",
-          padding: "1px",
-          borderRadius: "0 0 3px 0",
-          boxShadow: "inset 1px 1px 2px #00000044",
-          width: "20px",
-          height: "20px"
-        },
-        width: "15px",
-        height: "15px"
-      })}
-      <ListItemButton className="node-button" onClick={onClick}>
-        <ListItemText
-          primary={
-            <Typography fontSize="small">
-              {node.title}
-            </Typography>
-          }
-        />
-      </ListItemButton>
-    </div>
-  );
-});
+    return (
+      <div
+        className={`node ${isHovered ? "hovered" : ""}`}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        draggable
+        onDragStart={onDragStart}
+      >
+        {iconForType(outputType, {
+          fill: "#fff",
+          containerStyle: {
+            borderRadius: "0 0 3px 0",
+            marginLeft: "0.1em",
+            marginTop: "0"
+          },
+          bgStyle: {
+            backgroundColor: "#333",
+            margin: "0",
+            padding: "1px",
+            borderRadius: "0 0 3px 0",
+            boxShadow: "inset 1px 1px 2px #00000044",
+            width: "20px",
+            height: "20px"
+          },
+          width: "15px",
+          height: "15px"
+        })}
+        <ListItemButton className="node-button" onClick={onClick}>
+          <ListItemText
+            primary={<Typography fontSize="small">{node.title}</Typography>}
+          />
+        </ListItemButton>
+      </div>
+    );
+  }
+);
+
+NodeItem.displayName = "NodeItem";
 
 const groupNodes = (nodes: NodeMetadata[]) => {
   const groups: { [key: string]: NodeMetadata[] } = {};
-  nodes.forEach(node => {
+  nodes.forEach((node) => {
     if (!groups[node.namespace]) {
       groups[node.namespace] = [];
     }
@@ -108,8 +108,10 @@ const groupNodes = (nodes: NodeMetadata[]) => {
   return groups;
 };
 
-
-const RenderNodes: React.FC<RenderNodesProps> = ({ nodes, hoverDelay = 150 }) => {
+const RenderNodes: React.FC<RenderNodesProps> = ({
+  nodes,
+  hoverDelay = 150
+}) => {
   const { hoveredNode, setHoveredNode, setDragToCreate } = useNodeMenuStore(
     (state) => ({
       hoveredNode: state.hoveredNode,
@@ -131,48 +133,62 @@ const RenderNodes: React.FC<RenderNodesProps> = ({ nodes, hoverDelay = 150 }) =>
     hoverDelay
   );
 
-  const handleDragStart = useCallback((node: NodeMetadata) => (event: React.DragEvent<HTMLDivElement>) => {
-    setDragToCreate(true);
-    event.dataTransfer.setData("create-node", JSON.stringify(node));
-    event.dataTransfer.effectAllowed = "move";
-  }, [setDragToCreate]);
+  const handleDragStart = useCallback(
+    (node: NodeMetadata) => (event: React.DragEvent<HTMLDivElement>) => {
+      setDragToCreate(true);
+      event.dataTransfer.setData("create-node", JSON.stringify(node));
+      event.dataTransfer.effectAllowed = "move";
+    },
+    [setDragToCreate]
+  );
 
   const groupedNodes = useMemo(() => groupNodes(nodes), [nodes]);
 
-  const renderNode = useCallback((node: NodeMetadata) => {
-    const isHovered = hoveredNode?.node_type === node.node_type;
+  const renderNode = useCallback(
+    (node: NodeMetadata) => {
+      const isHovered = hoveredNode?.node_type === node.node_type;
 
-    return (
-      <NodeItem
-        key={`${node.namespace}-${node.title}`}
-        node={node}
-        isHovered={isHovered}
-        onMouseEnter={() => {
-          currentHoveredNodeRef.current = node;
-          handleMouseEnter();
-        }}
-        onMouseLeave={() => {
-          currentHoveredNodeRef.current = null;
-          handleMouseLeave();
-        }}
-        onDragStart={handleDragStart(node)}
-        onClick={() => handleCreateNode(node)}
-      />
-    );
-  }, [hoveredNode, handleMouseEnter, handleMouseLeave, handleDragStart, handleCreateNode]);
+      return (
+        <NodeItem
+          key={`${node.namespace}-${node.title}`}
+          node={node}
+          isHovered={isHovered}
+          onMouseEnter={() => {
+            currentHoveredNodeRef.current = node;
+            handleMouseEnter();
+          }}
+          onMouseLeave={() => {
+            currentHoveredNodeRef.current = null;
+            handleMouseLeave();
+          }}
+          onDragStart={handleDragStart(node)}
+          onClick={() => handleCreateNode(node)}
+        />
+      );
+    },
+    [
+      hoveredNode,
+      handleMouseEnter,
+      handleMouseLeave,
+      handleDragStart,
+      handleCreateNode
+    ]
+  );
 
   const elements = useMemo(() => {
-    return Object.entries(groupedNodes).flatMap(([namespace, nodesInNamespace]) => [
-      <Typography
-        key={`namespace-${namespace}`}
-        variant="h5"
-        component="div"
-        sx={{ my: 2, color: "#888" }}
-      >
-        {namespace}
-      </Typography>,
-      ...nodesInNamespace.map(renderNode)
-    ]);
+    return Object.entries(groupedNodes).flatMap(
+      ([namespace, nodesInNamespace]) => [
+        <Typography
+          key={`namespace-${namespace}`}
+          variant="h5"
+          component="div"
+          sx={{ my: 2, color: "#888" }}
+        >
+          {namespace}
+        </Typography>,
+        ...nodesInNamespace.map(renderNode)
+      ]
+    );
   }, [groupedNodes, renderNode]);
 
   return (
