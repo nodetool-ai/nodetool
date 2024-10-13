ae9758c97a84a85403a74586f6288f53058626d9
Matthias Georgi <matti.georgi@gmail.com>
2024-10-13 13:07:07 +0200

    use normal font size in nodes

--- web/src/components/node/PropertyLabel.tsx
+++ web/src/components/node/PropertyLabel.tsx
@@ -15,61 +15,39 @@ interface PropertyLabelProps {
   description?: string | null;
 }
 
-const labelStyles = (theme: any) =>
-  css({
-    '[role~="tooltip"][data-microtip-position|="bottom"]::before': {
-      background: "none"
-    },
-    '[role~="tooltip"][data-microtip-position|="bottom"]::after': {
-      position: "absolute",
-      bottom: "2.2em",
-      top: "unset",
-      left: "-3px",
-      transform: "none",
-      backgroundColor: theme.palette.c_gray1,
-      color: theme.palette.c_white,
-      fontFamily: theme.fontFamily1,
-      lineHeight: "1.2em",
-      padding: ".5em .75em",
-      display: "block",
-      maxWidth: "250px",
-      wordWrap: "break-word",
-      whiteSpace: "normal",
-      fontSize: theme.fontSizeTiny,
-      pointerEvents: "none",
-      transition:
-        "all var(--microtip-transition-duration) var(--microtip-transition-easing) .1s"
-    }
-  });
+const PropertyLabel: React.FC<PropertyLabelProps> = React.memo(
+  ({ id, name, description }) => {
+    const formattedName = useMemo(() => {
+      return name
+        .split("_")
+        .map(
+          (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        )
+        .join(" ");
+    }, [name]);
 
-const PropertyLabel: React.FC<PropertyLabelProps> = React.memo(({
-  id,
-  name,
-  description
-}) => {
-  const formattedName = useMemo(() => name.replaceAll("_", " "), [name]);
-
-  return (
-    <div className="property-label" css={labelStyles}>
-      {description ? (
-        <div
-          aria-label={description}
-          data-microtip-position="bottom"
-          role="tooltip"
-        >
-          <InputLabel draggable={false} htmlFor={id}>
+    return (
+      <div className="property-label">
+        {description ? (
+          <div
+            aria-label={description}
+            data-microtip-position="bottom"
+            role="tooltip"
+          >
+            <label draggable={false} htmlFor={id}>
+              {formattedName}
+            </label>
+          </div>
+        ) : (
+          <label draggable={false} htmlFor={id}>
             {formattedName}
-          </InputLabel>
-        </div>
-      ) : (
-        <InputLabel draggable={false} htmlFor={id}>
-          {formattedName}
-        </InputLabel>
-      )}
-    </div>
-  );
-});
+          </label>
+        )}
+      </div>
+    );
+  }
+);
 
-PropertyLabel.displayName = 'PropertyLabel';
+PropertyLabel.displayName = "PropertyLabel";
 
 export default PropertyLabel;
