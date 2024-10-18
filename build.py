1bd7ccdb66afc65a6355cbe610a7f00204872994
Matthias Georgi <mmg@meta.com>
2024-10-19 00:14:16 +0200

    fox ollama url

--- build.py
+++ build.py
@@ -228,7 +228,7 @@ class Build:
                 [
                     "curl",
                     "-L",
-                    "https://github.com/ollama/ollama/releases/download/v0.3.13/ollama-windows-amd64.zip"
+                    "https://github.com/ollama/ollama/releases/download/v0.3.13/ollama-windows-amd64.zip",
                     "-o",
                     "ollama.zip",
                 ]
@@ -263,7 +263,7 @@ class Build:
                 [
                     "curl",
                     "-L",
-                    "https://github.com/ollama/ollama/releases/download/v0.3.9/ollama-darwin",
+                    "https://github.com/ollama/ollama/releases/download/v0.3.13/ollama-darwin",
                     "-o",
                     str(self.BUILD_DIR / "ollama"),
                 ]
