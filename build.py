61146a5297e53a7882a83dfb3bae2e8c6d4b5741
Matthias Georgi <matti.georgi@gmail.com>
2024-11-18 08:55:10 +0100

    fix path in build

--- build.py
+++ build.py
@@ -418,7 +418,7 @@ class Build:
         venv_path = self.ENV_DIR / "venv"
         self.run_command(
             [
-                self.ENV_DIR / "bin" / "python",
+                str(self.ENV_DIR / "bin" / "python"),
                 "-m",
                 "venv",
                 str(venv_path),
