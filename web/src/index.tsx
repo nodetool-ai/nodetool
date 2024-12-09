6700324d928fdfe4b3e09a23c6a1820c0a2552cb
Matthias Georgi <matti.georgi@gmail.com>
2024-12-10 00:59:10 +0100

    remove web vitals

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 import { ReactFlowProvider, useStore } from "@xyflow/react";
 
 import {
@@ -237,8 +236,3 @@ root.render(
     </ReactFlowProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- web/src/reportWebVitals.ts
+++
@@ -1,9 +0,0 @@
-import { onCLS, onINP, onLCP } from "web-vitals";
-
-function reportWebVitals() {
-  onCLS(console.log);
-  onINP(console.log);
-  onLCP(console.log);
-}
-
-export default reportWebVitals;
