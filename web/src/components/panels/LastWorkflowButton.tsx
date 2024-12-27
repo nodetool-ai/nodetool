edcf66f55fd764398c212ca683881923e722ed3e
Matthias Georgi <mmg@meta.com>
2024-12-27 21:17:26 +0100

    fix workflow name input to be in sync

--- web/src/components/panels/LastWorkflowButton.tsx
+++ web/src/components/panels/LastWorkflowButton.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 
-import React, { useState, useCallback, useMemo } from "react";
+import React, { useState, useCallback, useMemo, useEffect } from "react";
 import { Button, IconButton, InputBase, Box, Tooltip } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import CheckIcon from "@mui/icons-material/Check";
@@ -53,7 +53,7 @@ const styles = (theme: any) =>
     "& .edit-button": {
       display: "block",
       position: "absolute",
-      left: "-.8em",
+      right: "-.8em",
       top: ".5em",
       width: "1em",
       height: "1em",
@@ -69,13 +69,14 @@ const styles = (theme: any) =>
       opacity: 1
     },
     "& .confirm-buttons": {
-      opacity: 0,
+      opacity: 1,
       position: "absolute",
       zIndex: 10,
-      top: "0",
-      right: "-2em",
-      width: "5em",
-      height: "1.5em",
+      top: "50%",
+      transform: "translateY(-50%)",
+      right: "-45px",
+      display: "flex",
+      gap: "4px",
       backgroundColor: "transparent"
     },
     "&:hover .confirm-buttons": {
@@ -90,14 +91,20 @@ const styles = (theme: any) =>
     },
     ".edit": {
       display: "flex",
-      alignItems: "flex-start"
+      alignItems: "center",
+      position: "relative",
+      backgroundColor: theme.palette.c_gray2,
+      borderRadius: "4px",
+      padding: "2px 8px",
+      minWidth: "200px",
+      border: `1px solid ${theme.palette.c_gray4}`
     },
     input: {
       fontSize: theme.fontSizeBig,
-      padding: ".5em 2em .2em 1em",
-      marginLeft: "3em",
+      padding: "6px 12px",
+      width: "100%",
       textAlign: "center",
-      backgroundColor: theme.palette.c_gray2,
+      backgroundColor: "transparent",
       border: "none",
       borderRadius: "0",
       color: theme.palette.c_white,
@@ -134,6 +141,12 @@ const LastWorkflowButton = React.memo(() => {
 
   const [localName, setLocalName] = useState(workflow?.name || "");
   const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    if (!isEditing) {
+      setLocalName(workflow?.name || "");
+    }
+  }, [workflow?.name, isEditing]);
 
   const handleNavigateToLastWorkflow = useCallback(() => {
     if (lastWorkflow) {
@@ -204,6 +217,7 @@ const LastWorkflowButton = React.memo(() => {
     <div className="last-workflow" css={styles}>
       {!isEditing ? (
         <>
+          {memoizedButton}
           <Tooltip title="Edit workflow name" enterDelay={TOOLTIP_ENTER_DELAY}>
             <IconButton
               className="edit-button"
@@ -214,7 +228,6 @@ const LastWorkflowButton = React.memo(() => {
               <EditIcon fontSize="small" />
             </IconButton>
           </Tooltip>
-          {memoizedButton}
         </>
       ) : (
         <Box className="edit">
