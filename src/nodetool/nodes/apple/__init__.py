efeb5d909d40ec5a9a10a51071629b1cf8eba1e3
Matthias Georgi <mmg@meta.com>
2024-12-31 12:33:00 +0100

    remove broken nodes

--- src/nodetool/nodes/apple/__init__.py
+++ src/nodetool/nodes/apple/__init__.py
@@ -1,5 +1,4 @@
 import nodetool.nodes.apple.calendar
 import nodetool.nodes.apple.messages
 import nodetool.nodes.apple.notes
-import nodetool.nodes.apple.reminders
 import nodetool.nodes.apple.speech
