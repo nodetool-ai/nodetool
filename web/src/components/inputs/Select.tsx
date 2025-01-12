064c4675e9cde166db5ea77f6babae40ae45c2d2
heavy-d <david@reaktant.com>
2025-01-12 01:56:06 +0100

    select bg lighter to better discern from string inputs

--- web/src/components/inputs/Select.tsx
+++ web/src/components/inputs/Select.tsx
@@ -86,7 +86,7 @@ const menuStyles = (theme: any) =>
     ".select-header": {
       padding: "0px 5px",
       margin: "5px 0",
-      backgroundColor: theme.palette.c_gray1,
+      backgroundColor: theme.palette.c_gray2,
       border: `1px solid ${theme.palette.c_gray2}`,
       fontSize: theme.fontSizeSmaller,
       borderRadius: ".3em",
