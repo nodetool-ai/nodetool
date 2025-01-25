47e1c013abe73d9241e6e8e165bfcf8a80b4c968
Matthias Georgi <matti.georgi@gmail.com>
2025-01-25 10:28:35 +0100

    Add WorkflowSelect component for workflow selection

---
+++ web/src/components/collections/WorkflowSelect.tsx
@@ -0,0 +1,63 @@
+import { Select } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import { WorkflowList } from "../../stores/ApiTypes";
+import { useQuery } from "@tanstack/react-query";
+import { useWorkflowStore } from "../../stores/WorkflowStore";
+import { memo } from "react";
+import { isEqual } from "lodash";
+
+interface WorkflowSelectProps {
+  id: string;
+  value?: { id: string };
+  onChange: (value: { type: "workflow"; id: string }) => void;
+}
+
+const WorkflowSelect = (props: WorkflowSelectProps) => {
+  const load = useWorkflowStore((state) => state.load);
+
+  const { data, error, isLoading } = useQuery<WorkflowList, Error>({
+    queryKey: ["workflows"],
+    queryFn: async () => {
+      return await load("", 200);
+    }
+  });
+
+  return (
+    <Select
+      id={props.id}
+      labelId={props.id}
+      name=""
+      value={props.value?.id || ""}
+      variant="standard"
+      onChange={(e) =>
+        props.onChange({
+          type: "workflow",
+          id: e.target.value
+        })
+      }
+      className="mui-select nodrag"
+      disableUnderline={true}
+      MenuProps={{
+        anchorOrigin: {
+          vertical: "bottom",
+          horizontal: "left"
+        },
+        transformOrigin: {
+          vertical: "top",
+          horizontal: "left"
+        }
+      }}
+    >
+      {isLoading && <MenuItem disabled>Loading...</MenuItem>}
+      {error && <MenuItem disabled>Error: {error.message}</MenuItem>}
+      {data?.workflows &&
+        data.workflows.map((workflow) => (
+          <MenuItem key={workflow.id} value={workflow.id}>
+            {workflow.name}
+          </MenuItem>
+        ))}
+    </Select>
+  );
+};
+
+export default memo(WorkflowSelect, isEqual);
