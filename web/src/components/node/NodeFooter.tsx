17fc2c10f2401d9ba5f4a875e9b93100dbd09da7
Matthias Georgi <matti.georgi@gmail.com>
2025-01-28 18:28:39 +0100

    Migrate node header and footer styles from CSS-in-JS to CSS

--- web/src/components/node/NodeFooter.tsx
+++ web/src/components/node/NodeFooter.tsx
@@ -45,77 +45,6 @@ export interface NodeFooterProps {
   showAdvancedFields?: boolean;
   onToggleAdvancedFields?: () => void;
 }
-export const footerStyles = (theme: any) =>
-  css({
-    display: "flex",
-    height: "18px",
-    alignItems: "flex-start",
-    background: theme.palette.c_node_header_bg,
-    borderRadius: "0 0 0.3em 0.3em",
-    overflow: "hidden",
-    ".namespace-button": {
-      display: "block",
-      margin: "0 0 0 0",
-      padding: "2px 10px",
-      borderRadius: 0,
-      backgroundColor: "transparent",
-      color: theme.palette.c_gray5,
-      fontFamily: theme.fontFamily1,
-      fontSize: theme.fontSizeSmall,
-      textTransform: "uppercase",
-      textAlign: "left",
-      flexGrow: 1,
-      overflow: "hidden"
-    },
-    ".namespace-button:hover": {
-      backgroundColor: "transparent",
-      color: theme.palette.c_hl1
-    },
-    ".namespace-button:hover .pretty-namespace span": {
-      color: theme.palette.c_hl1 + " !important"
-    },
-    ".help-button": {
-      display: "block",
-      padding: "0",
-      margin: "0 2px",
-      minWidth: "18px",
-      top: "-4px",
-      color: theme.palette.c_gray6,
-      backgroundColor: "transparent",
-      border: "none",
-      cursor: "pointer",
-      "&:hover": {
-        color: theme.palette.c_white
-      }
-    },
-    ".help-button svg": {
-      scale: "0.5"
-    },
-    ".advanced-fields-button": {
-      display: "block",
-      padding: "0",
-      margin: "0 2px",
-      minWidth: "18px",
-      top: "-4px",
-      right: "-7px",
-      color: theme.palette.c_gray6,
-      backgroundColor: "transparent",
-      border: "none",
-      cursor: "pointer",
-      transition: "transform 0.2s ease",
-      transformOrigin: "center center",
-      "&:hover": {
-        color: theme.palette.c_white
-      },
-      "&.active svg": {
-        color: theme.palette.c_hl1,
-        transform: "rotate(180deg)"
-      }
-    },
-    ".advanced-fields-button svg": {
-      scale: "0.7"
-    }
-  });
 
 export const NodeFooter: React.FC<NodeFooterProps> = ({
   nodeNamespace,
@@ -170,7 +99,7 @@ export const NodeFooter: React.FC<NodeFooterProps> = ({
   );
 
   return (
-    <div className="node-footer" css={footerStyles} style={{ backgroundColor }}>
+    <div className="node-footer" style={{ backgroundColor }}>
       <Tooltip
         title="Click to show in NodeMenu"
         placement="bottom-start"
