84843f28b4931f28a770a1a5b6eedc7237939eb8
Matthias Georgi <matti.georgi@gmail.com>
2025-01-28 18:28:39 +0100

    Migrate node header and footer styles from CSS-in-JS to CSS

--- web/src/components/node/NodeHeader.tsx
+++ web/src/components/node/NodeHeader.tsx
@@ -2,7 +2,7 @@
 import useContextMenuStore from "../../stores/ContextMenuStore";
 import { MoreHoriz } from "@mui/icons-material";
 import { css } from "@emotion/react";
-import { NodeStore, useNodeStore } from "../../stores/NodeStore";
+import { useNodeStore } from "../../stores/NodeStore";
 import { memo, useCallback, useMemo } from "react";
 import ThemeNodes from "../themes/ThemeNodes";
 import { isEqual } from "lodash";
@@ -22,120 +22,6 @@ export interface NodeHeaderProps {
   backgroundColor?: string;
 }
 
-export const headerStyle = (theme: any, hasParent: boolean) =>
-  css({
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    gap: 0,
-    width: "100%",
-    minHeight: "24px",
-    backgroundColor: hasParent
-      ? theme.palette.c_node_header_bg_group
-      : theme.palette.c_node_header_bg,
-    color: theme.palette.c_white,
-    margin: 0,
-    borderRadius: "0.3em 0.3em 0 0",
-    "& svg.MuiSvgIcon-root": {
-      scale: 0.8,
-      opacity: 1
-    },
-    "&:hover": {
-      transition: "opacity 0.15s",
-      color: theme.palette.c_gray6
-    },
-
-    ".node-title": {
-      display: "flex",
-      flexDirection: "column",
-      gap: 0,
-      flexGrow: 1,
-      textAlign: "left",
-      maxWidth: 170,
-      wordWrap: "break-word",
-      lineHeight: "1em",
-      color: theme.palette.c_white,
-      fontFamily: theme.fontFamily1,
-      fontSize: theme.fontSizeNormal,
-      fontFeatureSettings: '"smcp"',
-      padding: ".5em 0 .5em .7em",
-
-      "& .title-container": {
-        position: "absolute",
-        top: "-1.2em",
-        left: ".5em",
-        width: "100%",
-        minHeight: "1em",
-        backgroundColor: "transparent",
-        display: "flex",
-        flexDirection: "column",
-        gap: 0
-      },
-      "& .title": {
-        pointerEvents: "none",
-        position: "absolute",
-        bottom: "0",
-        left: ".2em",
-        width: "100%",
-        color: theme.palette.c_white,
-        fontSize: theme.fontSizeNormal,
-        lineHeight: "1.1em",
-        fontWeight: "300",
-        fontStyle: "italic",
-        margin: 0,
-        padding: 0
-      },
-      "& .type": {
-        pointerEvents: "none"
-      }
-    },
-    ".big": {
-      flex: 1,
-      maxWidth: 180,
-      wordWrap: "break-word",
-      lineHeight: "1em",
-      fontSize: 28
-    },
-    ".menu-button": {
-      height: "24px",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      marginLeft: "0.2em",
-      marginRight: "0.2em",
-      padding: 0,
-      color: theme.palette.c_gray6,
-      backgroundColor: "transparent",
-      border: "none",
-      cursor: "pointer",
-      "&:hover": {
-        color: theme.palette.c_white
-      }
-    },
-    ".menu-button svg": {
-      scale: "0.5"
-    },
-    ".color-picker-button": {
-      position: "absolute",
-      right: "0.5em",
-      pointerEvents: "all",
-      margin: "5px",
-      height: "1.1em",
-      zIndex: 10000,
-      backgroundColor: "transparent",
-      borderRadius: "0",
-      "& svg": {
-        color: theme.palette.c_gray5,
-        width: ".5em",
-        height: ".5em",
-        rotate: "-86deg"
-      },
-      "&:hover svg": {
-        color: theme.palette.c_hl1
-      }
-    }
-  });
-
 export const NodeHeader: React.FC<NodeHeaderProps> = ({
   id,
   metadataTitle,
@@ -149,11 +35,6 @@ export const NodeHeader: React.FC<NodeHeaderProps> = ({
   const titleizedType = useMemo(
     () => (metadataTitle ? titleizeString(metadataTitle) : ""),
     [metadataTitle]
-  );
-
-  const memoizedHeaderStyle = useMemo(
-    () => headerStyle(ThemeNodes, hasParent || false),
-    [hasParent]
   );
 
   const handleOpenContextMenu = useCallback(
@@ -170,10 +51,9 @@ export const NodeHeader: React.FC<NodeHeaderProps> = ({
     [id, openContextMenu]
   );
 
-  const updateNode = useNodeStore((state: NodeStore) => state.updateNode);
   const handleHeaderClick = useCallback(() => {
-    updateNode(id, { selected: true });
-  }, [updateNode, id]);
+    useNodeStore.getState().updateNode(id, { selected: true });
+  }, [id]);
 
   return (
     <Tooltip
@@ -182,8 +62,9 @@ export const NodeHeader: React.FC<NodeHeaderProps> = ({
       enterDelay={TOOLTIP_ENTER_DELAY}
     >
       <div
-        className="node-header"
-        css={memoizedHeaderStyle}
+        className={`node-drag-handle node-header ${
+          hasParent ? "has-parent" : ""
+        }`}
         onClick={handleHeaderClick}
         style={{
           backgroundColor: backgroundColor
@@ -199,7 +80,7 @@ export const NodeHeader: React.FC<NodeHeaderProps> = ({
         )}
         {!data.title && <span className="node-title">{titleizedType}</span>}
         {showMenu && (
-          <div className="menu-button" tabIndex={-1}>
+          <div className="menu-button-container" tabIndex={-1}>
             <button
               className="menu-button"
               tabIndex={-1}
