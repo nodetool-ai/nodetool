855156c617f55854cad71560d2576a706f391b9b
Matthias Georgi <matti.georgi@gmail.com>
2025-01-29 19:40:43 +0100

    fix broken import

--- web/src/stores/graphNodeToReactFlowNode.ts
+++ web/src/stores/graphNodeToReactFlowNode.ts
@@ -1,7 +1,7 @@
 import { Node } from "@xyflow/react";
 import { Workflow, Node as GraphNode } from "./ApiTypes";
 import { NodeData } from "./NodeData";
-import { NodeUIProperties, undefined, DEFAULT_NODE_WIDTH } from "./NodeStore";
+import { NodeUIProperties, DEFAULT_NODE_WIDTH } from "./NodeStore";
 
 export function graphNodeToReactFlowNode(
   workflow: Workflow,
