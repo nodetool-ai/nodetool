2489b3b567b662040c2d4eeb380aa97a5995ff37
Matthias Georgi <matti.georgi@gmail.com>
2025-02-02 13:12:33 +0100

    Implement Welcome screen routing and refactor AppHeader navigation
    - Add dedicated /welcome route with Welcome component
    - Remove welcome dialog and related logic from AppHeader
    - Update navigation buttons and routing in AppHeader
    - Modify index.tsx to handle welcome screen based on settings
    - Remove LastWorkflowButton component
    - Simplify AppHeaderStore by removing welcome-related methods

--- web/src/stores/AppHeaderStore.ts
+++ web/src/stores/AppHeaderStore.ts
@@ -1,34 +1,23 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 interface AppHeaderState {
-    helpOpen: boolean;
-    welcomeOpen: boolean;
-    helpIndex: number;
+  helpOpen: boolean;
+  helpIndex: number;
 
-    setHelpOpen: (open: boolean) => void;
-    setWelcomeOpen: (open: boolean) => void;
-    setHelpIndex: (index: number) => void;
+  setHelpOpen: (open: boolean) => void;
+  setHelpIndex: (index: number) => void;
 
-    handleOpenHelp: () => void;
-    handleCloseHelp: () => void;
-    handleOpenWelcome: () => void;
-    handleCloseWelcome: () => void;
+  handleOpenHelp: () => void;
+  handleCloseHelp: () => void;
 }
 
 export const useAppHeaderStore = create<AppHeaderState>((set, get) => ({
-    helpIndex: 0,
-    helpOpen: false,
-    welcomeOpen: false,
-    lastWorkflow: null,
-    workflowIsDirty: false,
-    statusMessage: '',
+  helpIndex: 0,
+  helpOpen: false,
 
-    setHelpOpen: (open) => set({ helpOpen: open }),
-    setWelcomeOpen: (open) => set({ welcomeOpen: open }),
-    setHelpIndex: (index) => set({ helpIndex: index }),
+  setHelpOpen: (open) => set({ helpOpen: open }),
+  setHelpIndex: (index) => set({ helpIndex: index }),
 
-    handleOpenHelp: () => set({ helpOpen: true }),
-    handleCloseHelp: () => set({ helpOpen: false }),
-    handleOpenWelcome: () => set({ welcomeOpen: true }),
-    handleCloseWelcome: () => set({ welcomeOpen: false }),
+  handleOpenHelp: () => set({ helpOpen: true }),
+  handleCloseHelp: () => set({ helpOpen: false })
 }));
