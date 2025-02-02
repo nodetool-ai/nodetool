983bed50881b13d015b74ba97a456c9391d26412
heavy-d <david@reaktant.com>
2025-02-02 02:43:03 +0100

    apps: add ComponentTest, prevent white bg on load

--- apps/index.tsx
+++ apps/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./components/App";
+import { ComponentTest } from "./components/ComponentTest";
 import "./index.css";
 import { Provider } from "./components/ui/provider";
 
 const urlParams = new URLSearchParams(window.location.search);
 const workflowId = urlParams.get("workflow_id");
+const isTestPage = urlParams.get("test") === "true";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
@@ -14,7 +16,11 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Provider>
-      <App initialWorkflowId={workflowId || undefined} />
+      {isTestPage ? (
+        <ComponentTest />
+      ) : (
+        <App initialWorkflowId={workflowId || undefined} />
+      )}
     </Provider>
   </React.StrictMode>
 );
