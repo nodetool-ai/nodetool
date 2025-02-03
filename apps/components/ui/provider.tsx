e2259bdf6bd31cdef4197e9ffd39ff5e3d5b92fc
heavy-d <david@reaktant.com>
2025-02-03 01:33:21 +0100

    Remove commented import and update ChakraProvider theme reference
    - Remove redundant commented import for apps_theme
    - Update ChakraProvider to use the correct theme import

--- apps/components/ui/provider.tsx
+++ apps/components/ui/provider.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ThemeProvider as NextThemeProvider } from "next-themes";
-import apps_theme from "../../styles/theme/apps_theme";
+// import theme from "../../styles/theme/apps_theme";
 import { ColorModeSync } from "./color-mode-sync";
 import theme from "../../styles/theme/apps_theme";
 
@@ -13,7 +13,7 @@ interface ProviderProps {
 
 export function Provider({ children }: ProviderProps) {
   return (
-    <ChakraProvider value={apps_theme}>
+    <ChakraProvider value={theme}>
       <NextThemeProvider attribute="class" defaultTheme="light" enableSystem>
         <ColorModeSync />
         {children}
