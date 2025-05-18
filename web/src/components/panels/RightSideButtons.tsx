6f16b7eff8577617126ff9acf98c443c22ae1bf7
heavy-d <david@reaktant.com>
2025-05-18 20:54:50 +0200

    replace theme color from brightest to white

--- web/src/components/panels/RightSideButtons.tsx
+++ web/src/components/panels/RightSideButtons.tsx
@@ -18,7 +18,7 @@ import ModelsManager from "../hugging_face/ModelsManager";
 const styles = (theme: any) =>
   css({
     ".command-icon": {
-      color: theme.palette.c_brightest
+      color: theme.palette.c_white
     }
   });
 
