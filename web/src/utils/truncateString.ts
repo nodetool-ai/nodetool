c8e23652acb75edc48db5d985e71201ca3803436
georgi <matti.georgi@gmail.com>
2025-05-21 10:23:08 +0200

    fix web tests

--- web/src/utils/truncateString.ts
+++ web/src/utils/truncateString.ts
@@ -1,6 +1,6 @@
 export function truncateString(str: string, maxLength: number = 50): string {
   if (str.length > maxLength) {
-    return str.slice(0, maxLength - 3) + "…";
+    return str.slice(0, maxLength - 1) + "…";
   }
   return str;
 }
