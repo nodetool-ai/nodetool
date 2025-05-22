0cb5d7fe94bad2a0ae7b1ffcb610ce7f4b37e6d5
georgi <matti.georgi@gmail.com>
2025-05-22 21:42:21 +0200

    test(apps): add store unit tests

--- apps/jest.setup.ts
+++ apps/jest.setup.ts
@@ -6,3 +6,19 @@ if (typeof globalThis.structuredClone !== 'function') {
     return JSON.parse(JSON.stringify(obj));
   };
 }
+
+// Polyfill TextEncoder/TextDecoder for jsdom environment
+try {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const util = require('util');
+  if (typeof globalThis.TextEncoder === 'undefined') {
+    // @ts-ignore
+    globalThis.TextEncoder = util.TextEncoder;
+  }
+  if (typeof globalThis.TextDecoder === 'undefined') {
+    // @ts-ignore
+    globalThis.TextDecoder = util.TextDecoder;
+  }
+} catch {
+  // ignore if util import fails
+}
