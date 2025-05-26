28f0f26e12b5df5576251f1a813bb0ea62489a07
heavy-d <david@reaktant.com>
2025-05-26 14:17:23 +0200

    gradient edges working but slow

--- web/src/components/node_editor/EdgeGradientDefinitions.tsx
+++ web/src/components/node_editor/EdgeGradientDefinitions.tsx
@@ -1,20 +1,46 @@
 import React from "react";
+import { DataType } from "../../config/data_types"; // Corrected import
 
-const EdgeGradientDefinitions: React.FC = () => {
-  return (
-    <svg style={{ height: 0, width: 0, position: "absolute" }}>
-      <defs>
-        <linearGradient id="edge-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+interface EdgeGradientDefinitionsProps {
+  dataTypes: DataType[];
+}
+
+const EdgeGradientDefinitions: React.FC<EdgeGradientDefinitionsProps> = ({
+  dataTypes
+}) => {
+  const gradients: JSX.Element[] = [];
+
+  // Gradients for connections
+  dataTypes.forEach((sourceType) => {
+    dataTypes.forEach((targetType) => {
+      if (sourceType.slug === targetType.slug) {
+        return; // Skip if types are the same
+      }
+      gradients.push(
+        <linearGradient
+          id={`gradient-${sourceType.slug}-${targetType.slug}`}
+          x1="0%"
+          y1="0%"
+          x2="100%"
+          y2="0%"
+          key={`grad-${sourceType.slug}-${targetType.slug}`}
+        >
           <stop
             offset="0%"
-            style={{ stopColor: "rgb(128,0,128)", stopOpacity: 1 }}
+            style={{ stopColor: sourceType.color, stopOpacity: 1 }}
           />
           <stop
             offset="100%"
-            style={{ stopColor: "rgb(255,192,203)", stopOpacity: 1 }}
+            style={{ stopColor: targetType.color, stopOpacity: 1 }}
           />
         </linearGradient>
-      </defs>
+      );
+    });
+  });
+
+  return (
+    <svg style={{ height: 0, width: 0, position: "absolute" }}>
+      <defs>{gradients}</defs>
     </svg>
   );
 };
