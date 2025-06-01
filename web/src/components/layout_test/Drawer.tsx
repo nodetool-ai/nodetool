1e86a34850afc72673f58e705fd25d7c53400728
heavy-d <david@friendlyvillagers.com>
2025-06-01 15:05:17 +0200

    Refine layout test components

---
+++ web/src/components/layout_test/Drawer.tsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { Drawer } from "@mui/material";
+
+const styles = { root: css`` };
+
+const DrawerDemo = () => <Drawer css={styles.root}>Drawer</Drawer>;
+
+export default DrawerDemo;
