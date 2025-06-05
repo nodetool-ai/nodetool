fe7c9fa8e793eaf6c61152a1b7dc3239c0bc4c62
heavy-d <david@reaktant.com>
2025-06-05 15:53:15 +0200

    refactor: improve animation for fitBounds and fitView in useFitView hook. now works consistently on first F key press

--- web/src/hooks/useFitView.ts
+++ web/src/hooks/useFitView.ts
@@ -9,7 +9,7 @@ export const useFitView = () => {
     selectedNodes: state.getSelectedNodes(),
     setSelectedNodes: state.setSelectedNodes
   }));
-
+  const TRANSITION_DURATION = 1000;
   return useCallback(() => {
     if (selectedNodes.length) {
       setTimeout(() => {
@@ -50,9 +50,37 @@ export const useFitView = () => {
         height: yMax - yMin + padding * 2
       };
 
-      reactFlowInstance.fitBounds(bounds, { duration: 1000, padding: 0.2 });
+      requestAnimationFrame(() => {
+        // First call, now with animation
+        reactFlowInstance.fitBounds(bounds, {
+          duration: TRANSITION_DURATION,
+          padding: 0.2
+        });
+
+        // Call it again after a very short delay, also with animation
+        setTimeout(() => {
+          reactFlowInstance.fitBounds(bounds, {
+            duration: TRANSITION_DURATION,
+            padding: 0.2
+          });
+        }, 10); // 10ms delay
+      });
     } else {
-      reactFlowInstance.fitView({ duration: 1000, padding: 0.1 });
+      requestAnimationFrame(() => {
+        // First call, now with animation
+        reactFlowInstance.fitView({
+          duration: TRANSITION_DURATION,
+          padding: 0.1
+        });
+
+        // Call it again after a very short delay, also with animation
+        setTimeout(() => {
+          reactFlowInstance.fitView({
+            duration: TRANSITION_DURATION,
+            padding: 0.1
+          });
+        }, 10); // 10ms delay
+      });
     }
   }, [nodes, selectedNodes, setSelectedNodes, reactFlowInstance]);
 };
