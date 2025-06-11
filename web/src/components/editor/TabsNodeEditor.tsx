d9cdbf64944b0ad7180423c9120b0a58d6fcfed3
heavy-d <david@reaktant.com>
2025-06-11 18:50:22 +0200

    fix: include openWorkflows in dependency array for useEffect in TabsNodeEditor to ensure correct updates

--- web/src/components/editor/TabsNodeEditor.tsx
+++ web/src/components/editor/TabsNodeEditor.tsx
@@ -341,7 +341,7 @@ const TabsNodeEditor = () => {
       return getNodeStore(currentWorkflowId);
     }
     return undefined;
-  }, [currentWorkflowId, getNodeStore]);
+  }, [currentWorkflowId, getNodeStore, openWorkflows]);
 
   // Create a combined list of tabs to render
   const tabsToRender = useMemo(() => {
