b3c6815fd659c6a2a2dc46cbe10eef3ef509b08b
georgi <matti.georgi@gmail.com>
2025-06-21 16:54:09 -0400

    cleanup

--- web/src/components/chat/containers/index.ts
+++ web/src/components/chat/containers/index.ts
@@ -1,4 +1,3 @@
 export { default as ChatView } from "./ChatView";
 export { default as GlobalChat } from "./GlobalChat";
-export { default as HelpChat } from "./HelpChat";
 export { default as WorkflowChat } from "./WorkflowChat";
