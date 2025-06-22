744453455b4d4ca9fd8d1eb75e96fdcf94d97713
heavy-d <david@reaktant.com>
2025-06-22 02:31:14 +0200

    refactor: Update PropertyLabel component and CSS styles - fixes style when label breaks over into second line - Replace hardcoded tooltip enter delay with constant from config

--- web/src/components/node/PropertyLabel.tsx
+++ web/src/components/node/PropertyLabel.tsx
@@ -3,6 +3,7 @@ import React, { memo, useMemo } from "react";
 import { titleizeString } from "../../utils/titleizeString";
 import { isEqual } from "lodash";
 import Tooltip from "@mui/material/Tooltip";
+import { TOOLTIP_ENTER_DELAY } from "../../config/constants";
 
 interface PropertyLabelProps {
   id: string;
@@ -21,7 +22,11 @@ const PropertyLabel: React.FC<PropertyLabelProps> = ({
 
   return (
     <div className="property-label">
-      <Tooltip title={description || ""} enterDelay={400} placement="left">
+      <Tooltip
+        title={description || ""}
+        enterDelay={TOOLTIP_ENTER_DELAY}
+        placement="left"
+      >
         <label draggable={false} htmlFor={id}>
           {formattedName}
         </label>
