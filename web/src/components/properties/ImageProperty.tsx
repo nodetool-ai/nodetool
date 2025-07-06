6f193786882b1a6d67ccd1e59efadbd2bb8ffa43
georgi <matti.georgi@gmail.com>
2025-07-06 18:44:18 +0200

    Enhance ImageProperty component to conditionally render dropzone
    - Added a check to determine if the image property is connected to any nodes, preventing the dropzone from displaying when connected.

--- web/src/components/properties/ImageProperty.tsx
+++ web/src/components/properties/ImageProperty.tsx
@@ -4,19 +4,28 @@ import { PropertyProps } from "../node/PropertyInput";
 import PropertyDropzone from "./PropertyDropzone";
 import { memo } from "react";
 import { isEqual } from "lodash";
+import { useNodes } from "../../contexts/NodeContext";
 
 const ImageProperty = (props: PropertyProps) => {
   const id = `image-${props.property.name}-${props.propertyIndex}`;
 
   const { asset, uri } = useAsset({ image: props.value });
 
+  const isConnected = useNodes((state) => {
+    return state.edges.some(
+      (edge) =>
+        edge.target == props.nodeId && edge.targetHandle === props.property.name
+    );
+  });
+
   return (
     <div className="image-property">
-        <PropertyLabel
-          name={props.property.name}
-          description={props.property.description}
-          id={id}
-        />
+      <PropertyLabel
+        name={props.property.name}
+        description={props.property.description}
+        id={id}
+      />
+      {!isConnected && (
         <PropertyDropzone
           asset={asset}
           uri={uri}
@@ -24,6 +33,7 @@ const ImageProperty = (props: PropertyProps) => {
           contentType="image"
           props={props}
         />
+      )}
     </div>
   );
 };
