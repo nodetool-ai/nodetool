b90a0bf92a22d52654f70de4f9991d66db73e953
georgi <matti.georgi@gmail.com>
2025-11-10 15:03:10 +0000

    Fix jest-dom matcher types in model formatting tests

--- web/src/utils/__tests__/modelFormatting.test.tsx
+++ web/src/utils/__tests__/modelFormatting.test.tsx
@@ -1,6 +1,7 @@
 /**
  * @jest-environment jsdom
  */
+import "@testing-library/jest-dom/jest-globals";
 import { describe, it, expect } from "@jest/globals";
 import { render, screen } from "@testing-library/react";
 import React from "react";
