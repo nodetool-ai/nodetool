154b159bb9688fba359600c2eb3e73aa124ab1d8
georgi <matti.georgi@gmail.com>
2025-12-05 13:18:18 -0500

    hide floating toolbar when model manager is open

--- web/src/stores/ModelManagerStore.ts
+++ web/src/stores/ModelManagerStore.ts
@@ -3,10 +3,12 @@ import { create } from "zustand";
 export type ModelFilterStatus = "all" | "downloaded" | "not_downloaded";
 
 interface ModelManagerState {
+  isOpen: boolean;
   modelSearchTerm: string;
   selectedModelType: string;
   maxModelSizeGB: number; // 0 means no limit
   filterStatus: ModelFilterStatus;
+  setIsOpen: (isOpen: boolean) => void;
   setModelSearchTerm: (term: string) => void;
   setSelectedModelType: (type: string) => void;
   setMaxModelSizeGB: (gb: number) => void;
@@ -14,10 +16,12 @@ interface ModelManagerState {
 }
 
 export const useModelManagerStore = create<ModelManagerState>((set) => ({
+  isOpen: false,
   modelSearchTerm: "",
   selectedModelType: "All",
   maxModelSizeGB: 0,
   filterStatus: "all",
+  setIsOpen: (isOpen) => set({ isOpen }),
   setModelSearchTerm: (term) => set({ modelSearchTerm: term }),
   setSelectedModelType: (type) => set({ selectedModelType: type }),
   setMaxModelSizeGB: (gb) => set({ maxModelSizeGB: gb }),
