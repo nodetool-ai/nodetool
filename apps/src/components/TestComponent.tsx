508644b5389b891490ca8c18109b17f1cca308db
georgi <matti.georgi@gmail.com>
2025-12-13 08:49:29 +0100

    remove apps

--- apps/src/components/TestComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface TestComponentProps {
-  text: string;
-}
-
-export const TestComponent: React.FC<TestComponentProps> = ({ text }) => {
-  return <div data-testid="test-component">{text}</div>;
-};
