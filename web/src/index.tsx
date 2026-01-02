1bf1beff4313a72e15822838823f763afee74308
georgi <matti.georgi@gmail.com>
2026-01-02 17:26:00 +0100

    add test route

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -97,6 +97,9 @@ const TemplateGrid = React.lazy(
   () => import("./components/workflows/ExampleGrid")
 );
 const LayoutTest = React.lazy(() => import("./components/LayoutTest"));
+const HTMLBuilderPanel = React.lazy(
+  () => import("./components/panels/HTMLBuilderPanel")
+);
 
 // Register frontend tools
 import "./lib/tools/builtin/addNode";
@@ -293,6 +296,16 @@ function getRoutes() {
       element: (
         <ProtectedRoute>
           <ModelListIndex />
+        </ProtectedRoute>
+      )
+    },
+    {
+      path: "html-builder",
+      element: (
+        <ProtectedRoute>
+          <PanelLeft />
+          <HTMLBuilderPanel />
+          <PanelBottom />
         </ProtectedRoute>
       )
     }
