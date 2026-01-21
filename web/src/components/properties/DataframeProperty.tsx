44d60951e4c76772039dd7873370d902be4b7a1c
heavy-d <david@reaktant.com>
2026-01-21 23:04:32 +0100

    Add DataframeRenderer and DataframeEditorModal components
    - Introduced DataframeRenderer for rendering DataFrame outputs with an expandable view.
    - Added DataframeEditorModal for editing DataFrame properties in a modal interface.
    - Updated OutputRenderer to utilize DataframeRenderer instead of DataTable.
    - Enhanced DataframeProperty to support opening the editor modal and managing DataFrame columns.

--- web/src/components/properties/DataframeProperty.tsx
+++ web/src/components/properties/DataframeProperty.tsx
@@ -2,15 +2,17 @@
 import { css } from "@emotion/react";
 import { useTheme } from "@mui/material/styles";
 import type { Theme } from "@mui/material/styles";
-import { memo, useCallback } from "react";
+import { memo, useCallback, useState } from "react";
 import { PropertyProps } from "../node/PropertyInput";
 import { ColumnDef, DataframeRef } from "../../stores/ApiTypes";
 import DataTable from "../node/DataTable/DataTable";
 import PropertyLabel from "../node/PropertyLabel";
 import ColumnsManager from "../node/ColumnsManager";
-import { Button, ButtonGroup } from "@mui/material";
+import DataframeEditorModal from "./DataframeEditorModal";
+import { Button, ButtonGroup, IconButton, Tooltip } from "@mui/material";
 // icons
 import TableRowsIcon from "@mui/icons-material/TableRows";
+import OpenInFullIcon from "@mui/icons-material/OpenInFull";
 import isEqual from "lodash/isEqual";
 
 const styles = (theme: Theme) =>
@@ -22,6 +24,27 @@ const styles = (theme: Theme) =>
     marginBottom: "0.5em",
     backgroundColor: "transparent",
     overflow: "hidden",
+    position: "relative",
+    ".property-header": {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      position: "relative"
+    },
+    ".dataframe-action-buttons": {
+      position: "absolute",
+      right: 0,
+      top: "-3px",
+      opacity: 0.8,
+      zIndex: 10
+    },
+    ".dataframe-action-buttons .MuiIconButton-root": {
+      margin: "0 0 0 5px",
+      padding: 0
+    },
+    ".dataframe-action-buttons .MuiIconButton-root svg": {
+      fontSize: "0.75rem"
+    },
     ".button-group": {
       display: "flex",
       marginBottom: "0.5em"
@@ -58,6 +81,19 @@ const DataframeProperty = ({
 }: PropertyProps) => {
   const theme = useTheme();
   const id = `${property.name}-${propertyIndex}`;
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const toggleExpand = useCallback(() => {
+    setIsExpanded((prev) => {
+      const next = !prev;
+      if (next) {
+        // Notify all other dataframe modals to close themselves
+        window.dispatchEvent(new Event("close-dataframe-editor-modal"));
+      }
+      return next;
+    });
+  }, []);
 
   const onCellChange = useCallback(
     (df: DataframeRef) => {
@@ -80,7 +116,7 @@ const DataframeProperty = ({
     const columns = value.columns || [];
     let newColumnName = "Column 1";
     let counter = 1;
-    while (columns.find((col: any) => col.name === newColumnName)) {
+    while (columns.find((col: ColumnDef) => col.name === newColumnName)) {
       newColumnName = `Column ${counter}`;
       counter++;
     }
@@ -97,29 +133,79 @@ const DataframeProperty = ({
 
   if (nodeType === "nodetool.constant.DataFrame") {
     return (
-      <div className="dataframe-editor" css={styles(theme)}>
-        <ButtonGroup className="button-group">
-          <Button className="add-column" onClick={addColumn}>
-            <TableRowsIcon style={{ rotate: "90deg" }} /> Add Column
-          </Button>
-        </ButtonGroup>
+      <div
+        className="dataframe-editor"
+        css={styles(theme)}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <div className="property-header">
+          <ButtonGroup className="button-group">
+            <Button className="add-column" onClick={addColumn}>
+              <TableRowsIcon style={{ rotate: "90deg" }} /> Add Column
+            </Button>
+          </ButtonGroup>
+          {isHovered && (
+            <div className="dataframe-action-buttons">
+              <Tooltip title="Open Editor" placement="bottom">
+                <IconButton size="small" onClick={toggleExpand}>
+                  <OpenInFullIcon />
+                </IconButton>
+              </Tooltip>
+            </div>
+          )}
+        </div>
         <ColumnsManager
           columns={value.columns || []}
           onChange={onChangeColumns}
           allData={value.data || []}
         />
         <DataTable dataframe={value} onChange={onCellChange} editable={true} />
+        {isExpanded && (
+          <DataframeEditorModal
+            value={value}
+            onChange={onChange}
+            onClose={toggleExpand}
+            propertyName={property.name}
+            propertyDescription={property.description}
+          />
+        )}
       </div>
     );
   } else {
     return (
-      <>
-        <PropertyLabel
-          name={property.name}
-          description={property.description}
-          id={id}
-        />
-      </>
+      <div
+        className="dataframe-property"
+        css={styles(theme)}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <div className="property-header">
+          <PropertyLabel
+            name={property.name}
+            description={property.description}
+            id={id}
+          />
+          {isHovered && (
+            <div className="dataframe-action-buttons">
+              <Tooltip title="Open Editor" placement="bottom">
+                <IconButton size="small" onClick={toggleExpand}>
+                  <OpenInFullIcon />
+                </IconButton>
+              </Tooltip>
+            </div>
+          )}
+        </div>
+        {isExpanded && (
+          <DataframeEditorModal
+            value={value}
+            onChange={onChange}
+            onClose={toggleExpand}
+            propertyName={property.name}
+            propertyDescription={property.description}
+          />
+        )}
+      </div>
     );
   }
 };
