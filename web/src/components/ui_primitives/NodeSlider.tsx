b94895e9191783f79343a4d3440a6962a9634f31
heavy-d <david@reaktant.com>
2026-01-25 04:36:08 +0100

    fix: integrate useEditorScope in NodeSlider component

--- web/src/components/ui_primitives/NodeSlider.tsx
+++ web/src/components/ui_primitives/NodeSlider.tsx
@@ -13,6 +13,7 @@
 import React, { forwardRef } from "react";
 import { Slider, SliderProps } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import { useEditorScope } from "../editor_ui";
 import { editorClassNames, cn } from "../editor_ui/editorUtils";
 
 export interface NodeSliderProps extends Omit<SliderProps, "size"> {
