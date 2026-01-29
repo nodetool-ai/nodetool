e03e956b92b17f74a9776b75b81382f00a7ebd33
heavy-d <david@reaktant.com>
2026-01-29 01:33:17 +0100

    Update styling in NodeHeader and editor controls f
    - Adjusted font weight in NodeHeader from 600 to 500 for a lighter appearance.
    - Modified editor control input styles, changing line height to 1.25em and font size to a smaller variant.
    - Updated editor background color in paletteDark.

--- web/src/components/themes/paletteDark.ts
+++ web/src/components/themes/paletteDark.ts
@@ -192,7 +192,7 @@ export const paletteDark: PaletteOptions = {
   c_node_header_bg_group: "#2A2A2A",
   c_bg_loop: "#2563EB22",
   c_bg_group: "#A1A4AD30",
-  c_editor_bg_color: "#232323",
+  c_editor_bg_color: "#1f2121",
   c_editor_grid_color: "#373737",
   c_editor_axis_color: "#2A2A2A",
   c_selection_rect: "#cdcdcd33",
