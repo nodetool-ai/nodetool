1c1135c87e1d88e0bd073a7cb5722f24f665d895
georgi <matti.georgi@gmail.com>
2026-04-17 17:35:24 +0200

    feat(trpc): implement settings router + tests

--- packages/websocket/src/trpc/router.ts
+++ packages/websocket/src/trpc/router.ts
@@ -1,12 +1,14 @@
 import { z } from "zod";
 import { router, publicProcedure } from "./index.js";
 import { costsRouter } from "./routers/costs.js";
+import { settingsRouter } from "./routers/settings.js";
 
 export const appRouter = router({
   healthz: publicProcedure.output(z.object({ ok: z.literal(true) })).query(() => ({
     ok: true as const
   })),
-  costs: costsRouter
+  costs: costsRouter,
+  settings: settingsRouter
 });
 
 export type AppRouter = typeof appRouter;
