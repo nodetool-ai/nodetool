b85f444d36381053846fdfa1fa0558e3ac945b4f
georgi <matti.georgi@gmail.com>
2026-04-17 17:45:13 +0200

    feat(protocol): add collections api-schemas

--- packages/protocol/src/api-schemas/index.ts
+++ packages/protocol/src/api-schemas/index.ts
@@ -1,4 +1,5 @@
 export * from "./api-error-code.js";
 export * from "./error-helpers.js";
+export * as collections from "./collections.js";
 export * as costs from "./costs.js";
 export * as settings from "./settings.js";
