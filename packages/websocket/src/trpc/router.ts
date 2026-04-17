8540a60b5793c0d5cc1233933ea3d4952a4f048f
georgi <matti.georgi@gmail.com>
2026-04-17 17:47:21 +0200

    feat(trpc): implement collections router + tests

--- packages/websocket/src/trpc/router.ts
+++ packages/websocket/src/trpc/router.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { router, publicProcedure } from "./index.js";
+import { collectionsRouter } from "./routers/collections.js";
 import { costsRouter } from "./routers/costs.js";
 import { settingsRouter } from "./routers/settings.js";
 
@@ -7,6 +8,7 @@ export const appRouter = router({
   healthz: publicProcedure.output(z.object({ ok: z.literal(true) })).query(() => ({
     ok: true as const
   })),
+  collections: collectionsRouter,
   costs: costsRouter,
   settings: settingsRouter
 });
