23f3b7db34b10fe05970766e7d5db1d0855b603d
georgi <matti.georgi@gmail.com>
2026-04-17 18:55:19 +0200

    feat(protocol): add assets api-schemas

--- packages/protocol/src/api-schemas/index.ts
+++ packages/protocol/src/api-schemas/index.ts
@@ -1,5 +1,6 @@
 export * from "./api-error-code.js";
 export * from "./error-helpers.js";
+export * as assets from "./assets.js";
 export * as collections from "./collections.js";
 export * as costs from "./costs.js";
 export * as fonts from "./fonts.js";
